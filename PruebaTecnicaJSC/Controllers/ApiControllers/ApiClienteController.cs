68c279d9cc706682db9163c4a094038102bc155a
Johan Cardona <jocardona1@outlook.es>
2022-05-22 10:39:44 -0500

    Cambio del tipo de proyecto web, y checkpoint de tiempo

---
+++ PruebaTecnicaJSC/Controllers/ApiControllers/ApiClienteController.cs
@@ -0,0 +1,31 @@
+﻿using Business.Business.AdminClientes;
+using Business.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PruebaTecnicaJSC.Controllers.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiClienteController : ControllerBase
+    {
+        private readonly IAdminClientesBusiness _business;
+
+        public ApiClienteController(IAdminClientesBusiness business)
+        {
+            _business = business;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cliente>>> Get()
+        {
+            List<Cliente> dataRetorno = new List<Cliente>();
+            var clientesConsultados = await _business.ConsultarClientes();
+
+            dataRetorno.AddRange(clientesConsultados);
+
+            return dataRetorno;
+        }
+
+    }
+}
--- PruebaTecnicaJSC/Program.cs
+++ PruebaTecnicaJSC/Program.cs
@@ -2,42 +2,42 @@ using Business.Business.AdminClientes;
 using Business.Interfaces;
 using Data.Database;
 using Data.Repository.AdminCliente;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using PruebaTecnicaJSC.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddSingleton<IPruebaDatabase>(x=> new
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<IPruebaDatabase>(x => new
     PruebaDatabase(builder.Configuration.GetConnectionString("PruebaTecnicaConectionString")));
 
-#region Inyección de dependencias
+#region Inyeccion de dependencias
 
 builder.Services.AddTransient<IAdminClienteRepository, AdminClienteRepository>();
 builder.Services.AddTransient<IAdminClientesBusiness, AdminClientesBusiness>();
 
 #endregion
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
+
 app.UseRouting();
-app.MapControllers();
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
